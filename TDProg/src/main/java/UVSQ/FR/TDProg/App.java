9c01ade3a20fd60184524810d302dddf3a342bc9
user <user@lubuntu>
2015-11-02 10:58:40 +0100

    nique sa mere

---
+++ TDProg/src/main/java/UVSQ/FR/TDProg/App.java
@@ -0,0 +1,13 @@
+package UVSQ.FR.TDProg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
