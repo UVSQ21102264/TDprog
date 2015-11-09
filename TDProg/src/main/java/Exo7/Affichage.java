b033ce817128513962e841eb4386b8e0b95fa985
user <user@lubuntu>
2015-11-09 12:42:14 +0100

    + exo 7

---
+++ TDProg/src/main/java/Exo7/Affichage.java
@@ -0,0 +1,9 @@
+package Exo7;
+
+import java.time.LocalDateTime;
+
+public class Affichage {
+	public void affiche(String mot) {
+		System.out.println(LocalDateTime.now() + mot);	
+	}
+}
---
+++ TDProg/src/main/java/Exo7/UneClasseMetier.java
@@ -0,0 +1,12 @@
+package Exo7;
+
+public class UneClasseMetier extends Affichage{
+	public void uneMethodeMetier() {
+		
+		affiche(": Début de uneMethodeMetier"); // log message
+		
+		// Traitement des metiers
+		
+		affiche(": Fin de uneMethodeMetier"); // log message
+	}
+}
